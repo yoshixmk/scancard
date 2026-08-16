a4db3a5f54d79f817034d40ba6a83234101fa562
Yoshihiro Ueki <pirori67@gmail.com>
2026-08-16 15:31:59 +0900

    build: Add Gradle build configuration and wrapper

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "9.3.1" apply false
+    id("com.android.library") version "9.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("com.google.dagger.hilt.android") version "2.60.1" apply false
+    id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
+}
