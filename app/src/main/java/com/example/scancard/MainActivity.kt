60ba8aef4a4e4e8bbdab5dae3825b1b7f25e8bbb
Yoshihiro Ueki <pirori67@gmail.com>
2026-08-16 15:32:07 +0900

    feat: Add application entry points

---
+++ app/src/main/java/com/example/scancard/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.scancard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.scancard.ui.ScanCardNavHost
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    ScanCardNavHost(navController = navController)
+                }
+            }
+        }
+    }
+}
